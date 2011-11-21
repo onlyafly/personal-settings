b20c01f4d98f41a2bbad6e633266a6014d4d428f
Kevin Albrecht <onlyafly@gmail.com>
2011-11-21 20:13:59 +0100

    Updated emacs init file

--- kevins-emacs-custom.el
+++ kevins-emacs-custom.el
@@ -1,8 +1,28 @@
-;; KEVIN ALBRECHT 2011-11-02
+;; KEVIN ALBRECHT 2011-11-21
 ;;
-;; .emacs should contain:
+;; .emacs or ~/.emacs.d/init.el should contain:
 ;;   (add-to-list 'load-path "~/code/personal-settings")
 ;;   (load-library "~/code/personal-settings/kevins-emacs-custom.el")
+
+;;---------- Initialize marmalade package manager
+
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+
+;;---------- Auto-install marmelade packages at startup if not found
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+;; Add in your own as you wish:
+(defvar my-packages '(starter-kit starter-kit-lisp)
+  "A list of packages to ensure are installed at launch.")
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
 
 ;;---------- VARIABLES
 
@@ -12,11 +32,23 @@
 
 ;;---------- FONT SETUP
 
-(set-face-attribute 'default nil :font "Bitstream Vera Sans Mono-10")
+(if is-system-windows
+    (set-face-attribute 'default nil :font "Bitstream Vera Sans Mono-10"))
+
+(if is-system-mac
+    (set-face-attribute 'default nil :font "Bitstream Vera Sans Mono-14"))
 
 (setq-default indent-tabs-mode nil)
 
 ;;---------- FILE SYSTEM
 
-(cd "C:/Users/Kevin/code/")
-(setq default-directory "C:/Users/Kevin/code/")
+(if is-system-windows
+    (progn
+      (cd "C:/Users/Kevin/code/")
+      (setq default-directory "C:/Users/Kevin/code/")))
+
+(if is-system-mac
+    (progn
+      (cd "~/code/")
+      (setq default-directory "~/code/")))
+
