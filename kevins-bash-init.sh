05d870a8be28a18e0e70c6833d17696e6100ad82
Kevin Albrecht <onlyafly@gmail.com>
2012-02-28 10:37:12 +0100

    Improved bash prompt to display Git status.

--- kevins-bash-init.sh
+++ kevins-bash-init.sh
@@ -42,11 +42,23 @@ alias gd='git diff'
 #PS1="[\d \t \u@\h: \w]\n$ "
 
 ## [~/code/test] $
-PS1="[\w] $ "
+#PS1="[\w] $ "
 
 ## Kevin@KEVIN-MSI ~/code/personal-settings (master)
 #PS1="\[\033]0;$MSYSTEM:\w\007 \033[32m\]\u@\h \[\033[33m\w$(__git_ps1)\033[0m\] $ "
 #PS1='\[\033]0;$MSYSTEM:\w\007\033[32m\]\u@\h \[\033[33m\w$(__git_ps1)\033[0m\] \n$ '
+
+# http://henrik.nyh.se/2008/12/git-dirty-prompt
+# http://www.simplisticcomplexity.com/2008/03/13/show-your-git-branch-name-in-your-prompt/
+#   username@Machine ~/dev/dir[master]$   # clean working directory
+#   username@Machine ~/dev/dir[master*]$  # dirty working directory
+function parse_git_dirty {
+  [[ $(git status 2> /dev/null | tail -n1) != "nothing to commit (working directory clean)" ]] && echo "*"
+}
+function parse_git_branch {
+  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/[\1$(parse_git_dirty)]/"
+}
+PS1='\[\033]0;$MSYSTEM:\w\007\033[32m\]\u@\h: \[\033[33m\w\033[0m\] \[\033[31m$(parse_git_branch)\033[0m\] \n$ '
 
 ##---------- Other
 
