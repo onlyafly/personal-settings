b7dd833c2716cea7c5750d7760c2a8fdc9e36bbb
Kevin Albrecht <onlyafly@gmail.com>
2021-06-15 17:28:40 +0200

    Update to ZSH

--- kevins-zsh-init.sh
+++ kevins-zsh-init.sh
@@ -5,16 +5,16 @@
 #
 # 1. Give this file execute permissions:
 #
-#    chmod +x kevins-bash-init.sh
+#    chmod +x kevins-zsh-init.sh
 #
 # 2. Add the following to your bash initialization file:
 #
-#    . ~/src/github.com/onlyafly/personal-settings/kevins-bash-init.sh
+#    . ~/src/github.com/onlyafly/personal-settings/kevins-zsh-init.sh
 #
 #    The bash initialization file will be found in one of these
 #    locations:
 #
-#    MacOS X: ~/.profile
+#    MacOS X: ~/.zshrc
 #
 #    Ubuntu:  ~/.bashrc
 #             (DO NOT place .profile, since .profile is run in the
@@ -47,17 +47,15 @@ alias gr='git rebase -i'
 
 ##---------- Custom prompt
 
-# http://henrik.nyh.se/2008/12/git-dirty-prompt
-# http://www.simplisticcomplexity.com/2008/03/13/show-your-git-branch-name-in-your-prompt/
-#   username@Machine ~/dev/dir[master]$   # clean working directory
-#   username@Machine ~/dev/dir[master*]$  # dirty working directory
-function parse_git_dirty {
-  [[ $(git status 2> /dev/null | tail -n1) != "nothing to commit, working tree clean" ]] && echo "*"
-}
-function parse_git_branch {
-  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/[\1$(parse_git_dirty)]/"
-}
-PS1='\[\033]0;$MSYSTEM:\w\007\033[32m\]\u@\h: \[\033[33m\w\033[0m\] \[\033[31m$(parse_git_branch)\033[0m\] \n$ '
+PROMPT='%2~ %# '
+
+autoload -Uz vcs_info
+precmd_vcs_info() { vcs_info }
+precmd_functions+=( precmd_vcs_info )
+setopt prompt_subst
+RPROMPT=\$vcs_info_msg_0_
+zstyle ':vcs_info:git:*' formats '%F{240}(%b)%r%f'
+zstyle ':vcs_info:*' enable git
 
 ##---------- Other
 
